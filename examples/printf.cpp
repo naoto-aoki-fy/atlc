7c406caad16dbb9084dd22f161e47e5f2147adcb
Naoto Aoki <184037301+naoto-aoki-fy@users.noreply.github.com>
2025-08-05 22:05:06 +0900

    STR_VAR_FLOAT -> ATLC_VAR_EQ, update Makefile

--- examples/printf.cpp
+++ examples/printf.cpp
@@ -2,15 +2,13 @@
 #include <cinttypes>
 #include <atlc/reorder.h>
 
-#define STR_VAR_INT(var) #var "=%" PRId64, (int64_t)var
-#define STR_VAR_UINT(var) #var "=%" PRIu64, (uint64_t)var
-#define STR_VAR_FLOAT(var) #var "=%g", (double)var
+#define ATLC_VAR_EQ(fmt, var) #var "=%" fmt, (var)
 
 int main() {
     int intvar = -1234;
-    int uintvar = 1234;
+    unsigned int uintvar = 1234;
     float floatvar = 1.234;
     double doublevar = 1.23456789;
-    printf(ATLC_REORDER("variables: " STR_VAR_INT(intvar), " " STR_VAR_UINT(uintvar), " " STR_VAR_FLOAT(floatvar), " " STR_VAR_FLOAT(doublevar), "\n"));
+    printf(ATLC_REORDER("variables: " ATLC_VAR_EQ("d", intvar), " " ATLC_VAR_EQ("u", uintvar), " " ATLC_VAR_EQ("f", floatvar), " " ATLC_VAR_EQ("lf", doublevar), "\n"));
     return 0;
 }
